8c46bb238224dce5fe3691ceb636ff1305f924b8
Narate Taerat <narate@ogc.us>
2022-05-02 13:15:31 +0000

    [WIP] csv (still wip)

--- example-1.sh
+++ example-1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+D=$(realpath $(dirname $0))
+
 for S in samp-{01..8}; do
 	./ldms-samp/run.sh --name ${S}
 done
@@ -9,8 +11,21 @@ done
 ./ldms-agg/run.sh --name agg-13 --prdcr "samp-{05,06}"
 ./ldms-agg/run.sh --name agg-14 --prdcr "samp-{07,08}"
 
-./ldms-agg/run.sh --name agg-21 --prdcr "agg-{11,12}"
-./ldms-agg/run.sh --name agg-22 --prdcr "agg-{13,14}"
+./ldms-agg/run.sh --name agg-21 --prdcr "agg-{11,12}" --strgp "meminfo loadavg"
+./ldms-agg/run.sh --name agg-22 --prdcr "agg-{13,14}" --strgp "meminfo loadavg"
+
+./ldms-agg/run.sh --name csv-21 --prdcr "agg-{11,12}" \
+		  --strgp-conf ${D}/csv/csv.conf \
+		  --interval 60000000 --offset 400000 \
+		  -v "${D}/scripts/ldmsd-conf:/opt/ovis/sbin/ldmsd-conf" \
+		  -v "${D}:/opt/ovis/etc/ldms/function_csv.conf" \
+		  -v "${D}/csv_store/csv-21:/store"
+./ldms-agg/run.sh --name csv-22 --prdcr "agg-{13,14}" \
+		  --strgp-conf ${D}/csv/csv.conf \
+		  --interval 60000000 --offset 400000 \
+		  -v "${D}/scripts/ldmsd-conf:/opt/ovis/sbin/ldmsd-conf" \
+		  -v "${D}:/opt/ovis/etc/ldms/function_csv.conf" \
+		  -v "${D}/csv_store/csv-22:/store"
 
 ./ldms-ui/run.sh --name ui --dsosd "agg-{21,22}"
 
--- ldms-agg/run.sh
+++ ldms-agg/run.sh
@@ -6,7 +6,7 @@ USAGE=$( cat <<EOF
 SYNOPSIS
     $0 [--name NAME] [--volume|-v HOST_PATH:CONT_PATH:OPT] [--conf PATH]
 	[--port LISTEN_PORT] [--samp "SAMPLER_PLUGIN_LIST"]
-	[--prdcr "PRDCR_LIST"] [--strgp "SCHEMA_LIST"]
+	[--prdcr "PRDCR_LIST"] [--strgp "SCHEMA_LIST"] [--strgp-conf CONF_FILE]
 	[--store-path STORE_PATH] [--interval USEC] [--offset USEC]
 	[--name LDMSD_NAME] [--compid NUM]
 
@@ -68,6 +68,11 @@ LDMSD-CONF-OPTIONS
 	Expansion" for more info). The option can be specified multiple times.
 	In such case, the lists will be concatinated together.
 
+    --strgp-conf CONF_FILE  (default: EMPTY)
+	The extra storages and storage policies configuration. If the file is
+	specified, the content of the file will be appended to the STRGP section
+	of the output.
+
     --store-path STORE_PATH   (default: "/store")
 	The 'path' option to store_sos plugin. Note that if the strgp
 	SCHEMA_LIST is empty, store_sos won't be loaded.
@@ -88,6 +93,8 @@ EOF
 NAME=agg
 IMG=ovishpc/ldms-agg
 CONF=
+STRGP_CONF=
+CONT_STRGP_CONF=/opt/ovis/etc/strgp.conf
 
 SAVED_ARGS=( )
 # Parse CLI options
@@ -121,6 +128,16 @@ while (( $# )); do
 		shift
 		VOLUMES+=( "$1" )
 		;;
+	--strgp-conf=* )
+		# STRGP_CONF will be mounted at /opt/ovis/etc/strgp.conf
+		STRGP_CONF=${1#--strgp-conf=}
+		SAVED_ARGS+=( "--strgp-conf" "${CONT_STRGP_CONF}" )
+		;;
+	--strgp-conf )
+		shift
+		STRGP_CONF=${1}
+		SAVED_ARGS+=( "--strgp-conf" "${CONT_STRGP_CONF}" )
+		;;
 	-h|--help )
 		cat <<<"$USAGE"
 		exit 0
@@ -145,13 +162,17 @@ for V in "${VOLUMES[@]}"; do
 	VOL_OPTS+=( "-v" "$V" )
 done
 
-STRGP_OPTS=( )
-for S in "${STRGP_SCHEMA_LIST[@]}"; do
-	STRGP_OPTS+=( --strgp "${S}" )
-done
+if [[ -n "${STRGP_CONF}" ]]; then
+	[[ -f "${STRGP_CONF}" ]] || {
+		echo "strgp-conf file not found: '${STRGP_CONF}'"
+		exit -1
+	}
+	STRGP_CONF=$(realpath ${STRGP_CONF})
+	VOL_OPTS+=( "-v" "${STRGP_CONF}:${CONT_STRGP_CONF}" )
+fi
 
 docker run -d --rm --name=${NAME} --hostname=${NAME} \
 	--network=${NET} \
 	"${VOL_OPTS[@]}" \
 	"${OPT_CAPADDS[@]}" \
-	${IMG} "${STRGP_OPTS[@]}" "$@"
+	${IMG} "$@"
